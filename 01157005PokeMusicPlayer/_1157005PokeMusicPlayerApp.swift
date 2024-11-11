654ab05e68303297ce7871f4ab8104c6ce92b6eb
Matthew930823 <matthew930823@gmail.com>
2024-11-11 16:32:40 +0800

    Initial Commit

---
+++ 01157005PokeMusicPlayer/_1157005PokeMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1157005PokeMusicPlayerApp.swift
+//  01157005PokeMusicPlayer
+//
+//  Created by user11 on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct _1157005PokeMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
